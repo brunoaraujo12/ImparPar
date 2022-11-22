946c8a94d14856077c064419765a0783cddc1244
Bruno Araujo <brunoaraujo-12@hotmail.com>
2022-11-22 18:18:52 -0300

    Adicionar arquivos de projeto.

---
+++ ImparPar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImparPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0;
+            double r = 0;
+
+            Console.WriteLine("Digite o numero a ser verificado: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            r = n % 2;
+
+            if (r == 1 )
+            {
+                Console.WriteLine("O número é impar");
+            }
+            else
+            {
+                Console.WriteLine("O número é par");
+            }
+
+            Console.ReadKey();
+
+
+            
+
+
+        }
+    }
+}
